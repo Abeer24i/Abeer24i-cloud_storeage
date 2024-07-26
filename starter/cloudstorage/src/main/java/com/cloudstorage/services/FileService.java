4ebcab48d1cc220c5de6c2ebf1ffed412fb4a4ca
Abeer <abeer.242sa@gmail.com>
2024-07-26 09:51:52 +0300

    Update FileService.java

--- starter/cloudstorage/src/main/java/com/cloudstorage/services/FileService.java
+++ starter/cloudstorage/src/main/java/com/cloudstorage/services/FileService.java
@@ -12,27 +12,22 @@ import java.util.List;
 
 @Service
 public class FileService {
-
     @Autowired
     private FileMapper fileMapper;
-
     @Autowired
     private UserMapper userMapper;
 
-    public String[] getFileListings(Integer userId) {
+    public List<File> getFileListings(Integer userId) {
         return fileMapper.getFileForUser(userId);
     }
-
 
     public File getAllFilebyid(Integer fileId) {
         return fileMapper.findFile(fileId);
     }
 
-
     public List<File> getAllFiles() {
         return fileMapper.findAllFiles();
     }
-
 
     public void saveFile(MultipartFile fileUpload, String userName) throws IOException {
         File existingFileByName = fileMapper.findFileByName(fileUpload.getOriginalFilename());
@@ -40,7 +35,6 @@ public class FileService {
             throw new IOException("File already exists");
         }
         Integer userId = userMapper.findUser(userName).getUserid();
-
         File file = new File();
         file.setFilename(fileUpload.getOriginalFilename());
         file.setContenttype(fileUpload.getContentType());
@@ -50,14 +44,7 @@ public class FileService {
         fileMapper.insert(file);
     }
 
-
     public void deleteFile(Integer fileId) {
         fileMapper.delete(fileId);
     }
 }
-
-
-
-
-
-
